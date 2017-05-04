63f4921b0f37de4a90a6aa9689957ef3b548906d
alexvahid <avahid@bu.edu>
2017-05-04 19:51:58 -0400

    cleaned the git

---
+++ Project Deliverable 4/ApiDemo/ApiDemoProj/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Alex\Dropbox\Senior\Software Engineering\ApiDemo\ApiDemoProj\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
